a5d365d0d50303248f47f02784ea683f0d09f91f
Leonidas Karagiannis <43794849+LeonLester@users.noreply.github.com>
2023-08-07 21:31:21 +0300

    recursive maze solution (2/5)

--- level_3/prepare-the-bunnies-escape/solution.py
+++ level_3/prepare-the-bunnies-escape/solution.py
@@ -1,73 +1,111 @@
+# Python3 code to implement the approach
 import sys
 
 
 # User defined Pair class
 class Pair:
-    def __init__(self, x, y):
-        self.first = x
-        self.second = y
+    def __init__(self, target_x, target_y):
+        self.target_x = target_x
+        self.target_y = target_y
 
 
-# Check if it is possible to go to (x, y) from the current
+HAVE_REMOVED_WALL = {"bool": False}
+
+
+# Check if it is possible to go to (target_x, target_y) from the current
 # position. The function returns false if the cell has
 # value 0 or already visited
-def isSafe(mat, visited, x, y):
-    return (
-        x >= 0
-        and x < len(mat)
-        and y >= 0
-        and y < len(mat[0])
-        # and mat[x][y] == 0
-        and (not visited[x][y])
+def isSafe(mat, visited, target_x, target_y):
+    # print(f"isSafe: {target_x},{target_y}")
+    standard_checks = (
+        target_x >= 0
+        and target_x < len(mat)
+        and target_y >= 0
+        and target_y < len(mat[0])
+        and (not visited[target_x][target_y])
     )
 
+    if standard_checks and (mat[target_x][target_y] == 0):
+        return True
 
-def findShortestPath(mat, visited, i, j, x, y, min_dist, dist):
-    if i == x and j == y:
+    elif (
+        standard_checks
+        and mat[target_x][target_y] == 1
+        and not HAVE_REMOVED_WALL["bool"]
+    ):
+        HAVE_REMOVED_WALL["bool"] = True
+        return True
+
+    elif standard_checks and (
+        mat[target_x][target_y] == 1 and HAVE_REMOVED_WALL["bool"]
+    ):
+        return False
+
+
+def findShortestPath(mat, visited, curr_x, curr_y, target_x, target_y, min_dist, dist):
+    # print(
+    #     f"findShortestPath with: ({curr_x},{curr_y}) ({target_x},{target_y}) {min_dist}, {dist}"
+    # )
+    if curr_x == target_x and curr_y == target_y:
         min_dist = min(dist, min_dist)
         return min_dist
 
-    # set (i, j) cell as visited
-    visited[i][j] = True
+    # set (curr_x, curr_y) cell as visited
+    visited[curr_x][curr_y] = True
 
     # go to the bottom cell
-    if isSafe(mat, visited, i + 1, j):
-        min_dist = findShortestPath(mat, visited, i + 1, j, x, y, min_dist, dist + 1)
+    if isSafe(mat, visited, curr_x + 1, curr_y):
+        min_dist = findShortestPath(
+            mat, visited, curr_x + 1, curr_y, target_x, target_y, min_dist, dist + 1
+        )
 
     # go to the right cell
-    if isSafe(mat, visited, i, j + 1):
-        min_dist = findShortestPath(mat, visited, i, j + 1, x, y, min_dist, dist + 1)
+    if isSafe(mat, visited, curr_x, curr_y + 1):
+        min_dist = findShortestPath(
+            mat, visited, curr_x, curr_y + 1, target_x, target_y, min_dist, dist + 1
+        )
 
     # go to the top cell
-    if isSafe(mat, visited, i - 1, j):
-        min_dist = findShortestPath(mat, visited, i - 1, j, x, y, min_dist, dist + 1)
+    if isSafe(mat, visited, curr_x - 1, curr_y):
+        min_dist = findShortestPath(
+            mat, visited, curr_x - 1, curr_y, target_x, target_y, min_dist, dist + 1
+        )
 
     # go to the left cell
-    if isSafe(mat, visited, i, j - 1):
-        min_dist = findShortestPath(mat, visited, i, j - 1, x, y, min_dist, dist + 1)
+    if isSafe(mat, visited, curr_x, curr_y - 1):
+        min_dist = findShortestPath(
+            mat, visited, curr_x, curr_y - 1, target_x, target_y, min_dist, dist + 1
+        )
 
-    # backtrack: remove (i, j) from the visited matrix
-    visited[i][j] = False
+    # backtrack: remove (curr_x, curr_y) from the visited matrix
+    visited[curr_x][curr_y] = False
     return min_dist
 
 
 # Wrapper over findShortestPath() function
 def solution(mat):
+    # if len(mat) == 0:
+    #     print("haaa")
+    #     return -1
+
     row = len(mat)
     col = len(mat[0])
+    src = Pair(0, 0)
+    dest = Pair(len(mat) - 1, len(mat[0]) - 1)
 
     # construct an `M × N` matrix to keep track of visited
     # cells
     visited = []
-    for i in range(row):
+    for _ in range(row):
         visited.append([None for _ in range(col)])
 
     dist = sys.maxsize
-    dist = findShortestPath(mat, visited, 0, 0, row, col, dist, 0)
+    # print(f"here2, {src.target_x}, {src.target_y}, {dest.target_x}, {dest.target_y}, {dist}")
+    dist = findShortestPath(
+        mat, visited, src.target_x, src.target_y, dest.target_x, dest.target_y, dist, 1
+    )
 
-    if dist != sys.maxsize:
-        return dist
-    return -1
-
-
-# This code is contributed by phasing17
+    # if dist != sys.maxsize:
+    return dist
+    # print(f"Dist is equal to maxsize")
+    # return -1
